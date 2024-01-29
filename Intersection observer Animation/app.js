1d1751e231d38d1d314077f1820ce51e91d90fad
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-01-29 08:16:41 +0600

    Implement animation using Intersection observer

---
+++ Intersection observer Animation/app.js
@@ -0,0 +1,14 @@
+let elements = document.querySelectorAll(".hidden");
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+elements.forEach((el) => observer.observe(el));
